565ac38cf8d3b94490d7ce09671b42df63a3cdb4
tobe-snapnet <tobe@snapnet.com.ng>
2023-06-15 22:12:02 +0100

    created product and category models, migrations and seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $category_names=['boots','sandals','sneakers'];
+        foreach($category_names as $category_name)
+        {
+            $category=Category::create(
+                [
+                    'name'=>$category_name,
+                    'discount'=>$category_name=='boots'??30
+                ]
+            );
+            if($category_name=='boots')
+            {
+                $category->products()->createMany(
+                [
+                    "sku" => "000001",
+                    "name" => "BV Lean leather ankle boots",
+                    "price" => 8900000
+                ],
+                [
+                    "sku" => "000002",
+                    "name" => "BV Lean leather ankle boots",
+                    "price" => 9900000
+                ],
+                [
+                    "sku" => "000003",
+                    "name" => "Ashlington leather ankle boots",
+                    "price" => 7100000,
+                    "discount"=>15
+                ]
+            );
+            }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            if($category_name=='sandals')
+            {
+                $category->products()->create(
+                [
+                    "sku" => "000004",
+                    "name" => "Naima embellished suede sandals",
+                    "category" => "sandals",
+                    "price" => 7950000
+                ]
+                );
+            }
+
+            if($category_name=='sneakers')
+            {
+                $category->products()->create(
+                    [
+                        "sku" => "000005",
+                        "name" => "Nathane leather sneakers",
+                        "price" => 5900000
+                    ]
+                )
+            }
+        }
     }
 }
